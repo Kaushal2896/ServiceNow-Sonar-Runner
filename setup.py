1d02b8db050181762e2e49682546dd08e50adf4e
W100-KAUSHALS\Aspire5 <kaushal.shah@crestdatasys.com>
2019-05-21 14:15:50 +0530

    made src dir

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 setuptools.setup(
     name="snownar",
     packages=['snownar'],
-    package_dir={'snownar': 'src'},
+    package_dir={'snownar': 'snownar'},
     version="0.0.2",
     author="kaushal28",
-    entry_points={'console_scripts': ['snownar = src.__main__:main' ]},
+    entry_points={'console_scripts': ['snownar = snownar.__main__:main' ]},
     author_email="shah.kaushal95@gmail.com",
     description="ServiceNow sonar uploader package",
     install_requires=[
