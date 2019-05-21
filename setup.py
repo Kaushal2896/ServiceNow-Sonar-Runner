06b977a626050959132a011a551a0e01072163f9
W100-KAUSHALS\Aspire5 <kaushal.shah@crestdatasys.com>
2019-05-21 13:57:14 +0530

    made src dir

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     package_dir={'ServiceNow-Sonar-Runner': 'src'},
     version="0.0.2",
     author="kaushal28",
-    entry_points={'console_scripts': ['ServiceNow-Sonar-Runner = src.__main__:main' ]}
+    entry_points={'console_scripts': ['ServiceNow-Sonar-Runner = src.__main__:main' ]},
     author_email="shah.kaushal95@gmail.com",
     description="ServiceNow sonar uploader package",
     install_requires=[
