aaeb3f7ec9133e0268c8e12c3e0e905651ff0cd5
W100-KAUSHALS\Aspire5 <kaushal.shah@crestdatasys.com>
2019-05-22 14:42:38 +0530

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
     name="snownar",
     packages=['snownar'],
     package_dir={'snownar': 'snownar'},
-    version="0.0.2",
+    version="0.0.3",
     author="kaushal28",
     entry_points={'console_scripts': ['snownar = snownar.__main__:main' ]},
     author_email="shah.kaushal95@gmail.com",
